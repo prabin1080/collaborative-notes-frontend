867a643c4c310413585c7326451cd07c56c102c4
Prabin <prabin1080@gmail.com>
2025-01-12 21:39:09 +0530

    redirect from homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,25 @@
+"use client";
+
 import Image from "next/image";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+
+
+
 
 export default function Home() {
+  const router = useRouter();
+  
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (!token) {
+      router.push("/auth/login");
+    } else {
+      router.push("/notes");
+    }
+  }, []);
+
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
